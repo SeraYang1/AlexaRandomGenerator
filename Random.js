a4bdb039fed8615d92cf417e7a2cb75e7639afd8
Sera Yang <sera.yang@berkeley.edu>
2017-01-26 00:10:04 -0800

    Still errors with literals

--- Random.js
+++ Random.js
@@ -32,11 +32,127 @@ exports.handler = (event, context) => {
                         
                             context.succeed(
                                 generateResponse(
-                                    buildSpeechletResponse(`Your random number is ${rand}`, false), {}
+                                    buildSpeechletResponse(`Your random number is ${rand}. Anything else?`, false), {}
                                 )
                             )
                         
                         break;
+
+                    case "two":
+                        var rand = Math.floor(Math.random() * 2);
+                        var val=" ";
+                        if (rand == 0){
+                            val = event.request.intent.slots.one.value;
+                        }
+                        else{
+                            val = event.request.intent.slots.two.value;
+                        }
+                            context.succeed(
+                                generateResponse(
+                                    buildSpeechletResponse(`Your random choice is ${val}. Anything else?`, false), {}
+                                )
+                            )
+                        
+                        break;
+
+                    case "three":
+                        var rand = Math.floor(Math.random() * 3);
+                        var val=" ";
+                        if (rand == 0){
+                            val = event.request.intent.slots.one.value.toLowerCase();
+                        }
+                        else if (rand == 1){
+                            val = event.request.intent.slots.two.value.toLowerCase();
+                        }
+                        else {
+                            val = event.request.intent.slots.three.value.toLowerCase();                            
+                        }
+                            context.succeed(
+                                generateResponse(
+                                    buildSpeechletResponse(`Your random choice is ${val}. Anything else?`, false), {}
+                                )
+                            )
+                        
+                        break;
+
+                    case "four":
+                        var rand = Math.floor(Math.random() * 4);
+                        var val=" ";
+                        if (rand == 0){
+                            val = event.request.intent.slots.one.value.toLowerCase();
+                        }
+                        else if (rand ==1){
+                            val = event.request.intent.slots.two.value.toLowerCase();
+                        }
+                        else if (rand == 2){
+                            val = event.request.intent.slots.three.value.toLowerCase();
+                        }
+                        else {
+                            val = event.request.intent.slots.four.value.toLowerCase();
+                        }
+                            context.succeed(
+                                generateResponse(
+                                    buildSpeechletResponse(`Your random choice is ${val}. Anything else?`, false), {}
+                                )
+                            )
+                        
+                        break;
+
+                    case "five":
+                        var rand = Math.floor(Math.random() * 5);
+                        var val=" ";
+                        if (rand == 0){
+                            val = event.request.intent.slots.one.value.toLowerCase();
+                        }
+                        else if (rand == 1){
+                            val = event.request.intent.slots.two.value.toLowerCase();
+                        }
+                        else if (rand == 2){
+                            val = event.request.intent.slots.three.value.toLowerCase();
+                        }
+                        else if (rand == 3){
+                            val = event.request.intent.slots.four.value.toLowerCase();
+                        }
+                        else {
+                            val = event.request.intent.slots.five.value.toLowerCase();
+                        }
+                            context.succeed(
+                                generateResponse(
+                                    buildSpeechletResponse(`Your random choice is ${val}. Anything else?`, false), {}
+                                )
+                            )
+                        
+                        break;
+
+                    case "six":
+                        var rand = Math.floor(Math.random() * 6);
+                        var val=" ";
+                        if (rand == 0){
+                            val = event.request.intent.slots.one.value.toLowerCase();
+                        }
+                        else if (rand == 1){
+                            val = event.request.intent.slots.two.value.toLowerCase();
+                        }
+                        else if (rand == 2){
+                            val = event.request.intent.slots.three.value.toLowerCase();
+                        }
+                        else if (rand == 3){
+                            val = event.request.intent.slots.four.value.toLowerCase();
+                        }
+                        else if (rand == 4){
+                            val = event.request.intent.slots.five.value.toLowerCase();
+                        }
+                        else{
+                            val = event.request.intent.slots.six.value.toLowerCase();
+                        }
+                            context.succeed(
+                                generateResponse(
+                                    buildSpeechletResponse(`Your random choice is ${val}. Anything else?`, false), {}
+                                )
+                            )
+                        
+                        break;
+
 
                         // default:
                         //     throw "Invalid intent"
